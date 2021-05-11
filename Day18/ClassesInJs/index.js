42e20fca72628095392efeb7512e95e3dbeb619e
umamaheswarara.tumma <umamaheswararao.tumma@servicenow.com>
2021-05-11 12:46:17 +0530

    Removed Extra spaces

--- Day18/ClassesInJs/index.js
+++ Day18/ClassesInJs/index.js
@@ -1,12 +1,8 @@
 const Vehicle = require("./class");
 var bike = new Vehicle(2, 1234, 2);
 console.log(bike);
-
 var car = new Vehicle(4, 2345, 4);
 console.log(car);
-
 bike.paint("Red");
-
 car.paint("Black");
-
 console.log(bike, car);
